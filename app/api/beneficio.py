c8a8318fb79e2783d560cd960733bcd350744723
Vítor Albuquerque Brando <vitorbrando@gmail.com>
2023-02-28 20:08:48 -0300

    Consulta ExtratoClube

--- app/api/beneficio.py
+++ app/api/beneficio.py
@@ -1,3 +1,37 @@
+from .crawler_extratoclube import get_auth_token
+from .consts import URL_EXTRATO
+from flask import Response
+import requests, json
+
+tokens_cache = {}
+
 def consulta_cpf(consulta):
-    print(consulta)
-    return ''
+    global tokens_cache
+    cpf = consulta['cpf']
+    usuario = consulta['usuario']
+    senha = consulta['senha']
+
+    if tokens_cache.get(usuario) == None:
+        tokens_cache[usuario] = get_auth_token(usuario, senha)
+      
+    endpoint = "{}{}{}{}".format("http://", URL_EXTRATO, "/offline/listagem/", cpf)
+    headers = {"Authorization": tokens_cache[usuario]}
+
+    resp = requests.get(endpoint, headers=headers)
+    if resp.status_code == 200:
+        try:
+            resp_json = resp.json()
+        except:
+            resp_json = None
+
+        if resp_json != None:
+            if len(resp_json['beneficios']) > 1:
+                nb = []
+                for b in resp_json['beneficios']:
+                    nb.append(b['nb'])
+            else:
+                nb = (resp.json()['beneficios'][0]['nb'])
+            return Response(json.dumps({"result": nb}), status=200, mimetype='application/json')
+
+    tokens_cache[usuario] = None
+    return Response(json.dumps({"result": "Falha ao buscar informação"}), status=400, mimetype='application/json')
---
+++ app/api/crawler_extratoclube.py
@@ -0,0 +1,34 @@
+import time
+from .consts import URL_TOKEN_LOGIN, URL_EXTRATO
+from seleniumwire import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+def get_auth_token(usuario, senha):
+    options = webdriver.ChromeOptions()
+    options.add_argument("--headless")
+    driver = webdriver.Chrome(chrome_options=options)
+    driver.get("{}{}".format("http://", URL_TOKEN_LOGIN))
+
+    username = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, 'user')))
+    password = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, 'pass')))
+
+    username.send_keys(usuario)
+    password.send_keys(senha)
+
+    total_requests = len(driver.requests)
+    botao = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, 'botao')))
+    botao.click()
+
+    while (len(driver.requests) <= total_requests):
+        time.sleep(1)
+
+    token = None
+    for r in driver.requests:
+        if r.method == 'POST' and r.host == URL_EXTRATO:
+            if (r.response):
+                token = r.response.headers['Authorization']
+            break
+
+    return token
